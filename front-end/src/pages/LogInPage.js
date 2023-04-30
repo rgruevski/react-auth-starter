5633c5613823daaaa9563be2f13a1913479854fc
rgruevski <robgruevski@gmail.com>
2023-04-30 19:27:31 -0400

    Login Page & Route 0727

---
+++ front-end/src/pages/LogInPage.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+export const LogInPage = () => {
+
+    const [errorMessage, setErrorMessage] = useState("");
+    const [emailValue, setEmailValue] = useState("");
+    const [passwordValue, setPasswordValue] = useState("");
+
+    const history = useHistory();
+
+    const onLogInClicked = async () => {
+        alert('Log in not implemented yet');
+    }
+
+    return (
+        <div className="content-container">
+            {
+                errorMessage &&
+                <div className="fail">{errorMessage}</div>
+            }
+            <input
+                type="email"
+                placeholder="Type Email..."
+                onChange={e => setEmailValue(e.target.value)} />
+            <input
+                type="password"
+                placeholder="Type Password..."
+                onChange={e => setPasswordValue(e.target.value)} />
+            <button
+                onClick={onLogInClicked}
+                disabled={!emailValue || !passwordValue}>
+                Log In
+            </button>
+            <button
+                onClick={() => history.push('/forgot-password')}>
+                Forgot Your Password?
+            </button>
+            <button
+                onClick={() => history.push('/signup')}>
+                Don't have an account? Sign Up
+            </button>
+        </div>
+    );
+};
