aaa73e1daeffd7c3a281319e8f4a605bf13bc2ca
Robert Gruevski <robgruevski@gmail.com>
2023-05-01 11:18:28 -0400

    useToken useUser changes

--- front-end/src/auth/useToken.js
+++ front-end/src/auth/useToken.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 export const useToken = () => {
-    const [token, setTokenInternal] = useState({});
-    
+    const [token, setTokenInternal] = useState(() => {
+        return localStorage.getItem('token');
+    });
+    const setToken = newToken => {
+        localStorage.setItem('token', newToken);
+        setTokenInternal(newToken);
+    }
+    return [token, setToken];
 };
---
+++ front-end/src/auth/useUser.js
@@ -0,0 +1,17 @@
+import {useState,useEffect} from 'react';
+import { useToken } from './useToken';
+export const useUser = () => {
+    const [token] = useToken();
+
+    // Getting the 'Payload' from the Token and parsing it into a JSON.
+    const getPayloadFromToken = (token) => {
+        const encodedPayload = token.split('.')[1];
+        return JSON.parse(atob(encodedPayload));
+    }
+
+    // Define the User State.
+    const [user, setUser] = useState(() => {
+        if (!token) return null;
+        return getPayloadFromToken(token);
+    })s
+}
