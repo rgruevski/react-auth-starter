c9880119aa9ccbc3c80da8a52e6dd6d6543aa4c8
rgruevski <robgruevski@gmail.com>
2023-05-17 21:39:03 -0400

    Creating a reset password landing

--- front-end/src/pages/EmailVerificationSuccess.js
+++ front-end/src/pages/EmailVerificationSuccess.js
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import { useHistory } from 'react-router-dom';
-
-export const EmailVerificationSuccess = () => {
-    const history = useHistory();
-
-=======
 import { useHistory } from "react-router-dom";
 export const EmailVerificationSuccess = () => {
     const history = useHistory();
->>>>>>> 6449e63 (Email Verification)
     return (
         <div className="content-container">
             <h1>Success!</h1>
             <p>
-<<<<<<< HEAD
-                Thanks for verifying your email, now you can use all the app's features.
-=======
-                Thanks for verifying your email, now you can use every feature.
->>>>>>> 6449e63 (Email Verification)
-            </p>
+                Thanks for verifying your email, now you can use all of the app's features.
+            </p>s
             <button onClick={() => history.push('/')}>Go to home page</button>
         </div>
     );
---
+++ front-end/src/pages/PasswordResetFail.js
@@ -0,0 +1,13 @@
+import { useHistory } from "react-router-dom";
+export const PasswordVerificationFail = () => {
+    const history = useHistory();
+    return (
+        <div className="content-container">
+            <h1>Uh oh..!</h1>
+            <p>
+                There was an issue resetting your password.
+            </p>
+            <button onClick={() => history.push('/login')}>Go to LogIn</button>
+        </div>
+    );
+}
